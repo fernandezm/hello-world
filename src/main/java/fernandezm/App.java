b8b192c32870af3115dd8bce2cc147e5fbf156a9
Miguel Angel Fernandez <miguelangelfr@gmail.com>
2019-01-03 00:20:40 +0100

    Compliant sonar - Reorder modifiers

--- src/main/java/fernandezm/App.java
+++ src/main/java/fernandezm/App.java
@@ -8,7 +8,7 @@ import org.apache.log4j.Logger;
  */
 public class App 
 {
-	private final static Logger logger = Logger.getLogger(App.class);
+	private static final Logger logger = Logger.getLogger(App.class);
 	
 	public static void main( String[] args )
     {
